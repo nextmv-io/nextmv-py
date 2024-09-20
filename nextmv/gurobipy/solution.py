d815f808b2ef02adbfe6af2d135386ae055d9ca3
Ryan O'Neil <6748953+ryanjoneil@users.noreply.github.com>
2024-09-20 11:50:05 -0400

    GurobipySolution inherits from BaseModel.

--- nextmv/gurobipy/solution.py
+++ nextmv/gurobipy/solution.py
@@ -1,18 +1,22 @@
 import gurobipy as gp
+from pydantic import ConfigDict
+
+from nextmv.base_model import BaseModel
 
 
-class GurobipySolution:
+class GurobipySolution(BaseModel):
     """Gurobipy solution representation."""
 
-    def __init__(self, model: gp.Model):
-        self._model = model
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    model: gp.Model = None
 
     @classmethod
     def from_model(cls, model: gp.Model):
-        return cls(model)
+        return cls(model=model)
 
     def to_dict(self):
-        if self._model.SolCount < 1:
+        if self.model.SolCount < 1:
             return None
 
-        return {x.VarName: x.X for x in self._model.getVars()}
+        return {x.VarName: x.X for x in self.model.getVars()}
