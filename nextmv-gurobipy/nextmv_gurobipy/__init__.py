b2766afd979436c73b18bd7d5467be9c0d6121df
Sebastian Quintero <sebastianquinterorojas@gmail.com>
2025-02-24 16:14:15 -0500

    Change Statistics to ModelStatistics and add tests

--- nextmv-gurobipy/nextmv_gurobipy/__init__.py
+++ nextmv-gurobipy/nextmv_gurobipy/__init__.py
@@ -4,7 +4,7 @@ from .__about__ import __version__
 from .model import Model as Model
 from .options import ModelOptions as ModelOptions
 from .solution import ModelSolution as ModelSolution
-from .statistics import Statistics as Statistics
+from .statistics import ModelStatistics as ModelStatistics
 
 VERSION = __version__
 """The version of the Nextmv & Gurobi Python SDK."""
