6134812d0fb08d9ae783e5f8eab144a4b32b6adc
Sebastian Quintero <sebastianquinterorojas@gmail.com>
2025-04-18 22:54:43 -0500

    Return CSV, was deleted by accident

--- nextmv/nextmv/input.py
+++ nextmv/nextmv/input.py
@@ -19,6 +19,8 @@ class InputFormat(str, Enum):
     """JSON format, utf-8 encoded."""
     TEXT = "text"
     """Text format, utf-8 encoded."""
+    CSV = "csv"
+    """CSV format, utf-8 encoded."""
     CSV_ARCHIVE = "csv-archive"
     """CSV archive format: multiple CSV files."""
 
