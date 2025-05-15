a3453959cc5ca84aac53caed68bca80850ca4e57
Marius Merschformann <marius.merschformann@gmail.com>
2025-05-15 22:02:59 +0200

    Stabilize tests

--- nextmv/tests/cloud/test_run.py
+++ nextmv/tests/cloud/test_run.py
@@ -8,9 +8,9 @@ from nextmv.cloud.run import run_duration
 class TestRunDuration(unittest.TestCase):
     def test_run_duration_convenience_func(self):
         before_t, before_dt = time.time(), datetime.datetime.now()
-        time.sleep(0.25)
-        after_t, after_dt = time.time(), datetime.datetime.now()
+        diff = 0.25
+        after_t, after_dt = before_t + diff, before_dt + datetime.timedelta(seconds=diff)
         duration_t = run_duration(before_t, after_t)
         duration_dt = run_duration(before_dt, after_dt)
-        self.assertAlmostEqual(duration_t, 250.0, delta=20.0)
-        self.assertAlmostEqual(duration_dt, 250.0, delta=20.0)
+        self.assertAlmostEqual(duration_t, 250.0, delta=0.1)
+        self.assertAlmostEqual(duration_dt, 250.0, delta=0.1)
