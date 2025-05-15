aa1b780b4ac4bcadeb1511e45daa8d99381e6c1b
Marius Merschformann <marius.merschformann@gmail.com>
2025-05-15 21:33:45 +0200

    Speed up test, relax assertion

--- nextmv/tests/cloud/test_run.py
+++ nextmv/tests/cloud/test_run.py
@@ -8,9 +8,9 @@ from nextmv.cloud.run import run_duration
 class TestRunDuration(unittest.TestCase):
     def test_run_duration_convenience_func(self):
         before_t, before_dt = time.time(), datetime.datetime.now()
-        time.sleep(1)
+        time.sleep(0.25)
         after_t, after_dt = time.time(), datetime.datetime.now()
         duration_t = run_duration(before_t, after_t)
         duration_dt = run_duration(before_dt, after_dt)
-        self.assertAlmostEqual(duration_t, 1000.0, delta=10.0)
-        self.assertAlmostEqual(duration_dt, 1000.0, delta=10.0)
+        self.assertAlmostEqual(duration_t, 250.0, delta=20.0)
+        self.assertAlmostEqual(duration_dt, 250.0, delta=20.0)
