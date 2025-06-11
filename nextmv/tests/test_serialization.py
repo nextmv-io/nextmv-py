029f24313dd8f1fa306b105c38b6a1a751476f2c
Marius Merschformann <marius.merschformann@gmail.com>
2025-06-12 00:58:05 +0200

    Adding serialization docs

--- nextmv/tests/test_serialization.py
+++ nextmv/tests/test_serialization.py
@@ -28,7 +28,7 @@ class TestSerialization(unittest.TestCase):
         self.assertEqual(serialized, expected)
 
     def test_default_deflated_serialization(self):
-        """Test the default serialization"""
+        """Test the default deflated serialization"""
 
         data = {
             "name": "Test",
@@ -68,5 +68,29 @@ class TestSerialization(unittest.TestCase):
             },
             indent=2,
             separators=(",", ": "),
+        )
+        self.assertEqual(serialized, expected)
+
+    def test_compressed_serialization(self):
+        """Test a requested compressed serialization"""
+
+        data = {
+            "name": "Test",
+            "value": 42,
+            "timestamp": nextmv._serialization._custom_serial(datetime.datetime(2023, 10, 1)),
+        }
+        json_configurations = {
+            "separators": (",", ":"),  # Remove spaces for a compressed format
+            "indent": None,  # No indentation for compressed format
+        }
+        serialized = nextmv._serialization.serialize_json(data, json_configurations)
+        expected = json.dumps(
+            {
+                "name": "Test",
+                "value": 42,
+                "timestamp": "2023-10-01T00:00:00",
+            },
+            separators=(",", ":"),
+            indent=None,
         )
         self.assertEqual(serialized, expected)
