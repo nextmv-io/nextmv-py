9d2fc721d146966e8d0b7e80c42d304c69101194
Sebastian Quintero <sebastianquinterorojas@gmail.com>
2025-10-24 12:11:47 -0500

    Reads entire log file
    Reads the entire log file at once.

--- nextmv/nextmv/local/application.py
+++ nextmv/nextmv/local/application.py
@@ -1045,7 +1045,7 @@ class Application:
         # Read the logs of the run and place each line as an element in a list
         run_dir = os.path.join(runs_dir, run_id)
         with open(os.path.join(run_dir, LOGS_KEY, LOGS_FILE)) as f:
-            stderr_logs = [line.rstrip("\n") for line in f.readlines()]
+            stderr_logs = f.read()
 
         # Create the tracked run object and start configuring it.
         tracked_run = TrackedRun(
