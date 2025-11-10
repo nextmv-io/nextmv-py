92fbc2a25c50446d7a8423774352a7f628093cac
Marius Merschformann <marius.merschformann@gmail.com>
2025-11-10 23:22:19 +0100

    Remove obsolete init

--- nextmv/nextmv/cloud/package.py
+++ nextmv/nextmv/cloud/package.py
@@ -205,7 +205,6 @@ def __find_files(
 def __confirm_mandatory_files(manifest: Manifest, present_files: list[str]) -> None:
     """Confirm that all mandatory files are present in the given list of files."""
 
-    missing_files = []
     found_files = {os.path.normpath(file): True for file in present_files}
 
     # Check for mandatory files (if a custom execution config is provided we check the
