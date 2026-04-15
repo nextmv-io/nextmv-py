67282dc17b5d7daa74438f76fdff0c27b2f6464a
Marius Merschformann <marius.merschformann@gmail.com>
2026-04-15 16:44:18 +0200

    Unexporting find uv

--- nextmv/nextmv/local/__init__.py
+++ nextmv/nextmv/local/__init__.py
@@ -5,4 +5,3 @@ Functionality for locally simulating the Nextmv Cloud.
 from .application import Application as Application
 from .registry import AppEntry as AppEntry
 from .registry import Registry as Registry
-from .uv_handler import _find_uv_binary as _find_uv_binary
