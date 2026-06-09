4dd12d9f7288df55c30ecd555d0c881e1433124e
Sebastian Quintero <sebastianquinterorojas@gmail.com>
2026-06-09 12:42:50 -0500

    Adds pagination to listing batch exps

--- nextmv/nextmv/cli/cloud/batch/list.py
+++ nextmv/nextmv/cli/cloud/batch/list.py
@@ -9,7 +9,7 @@ import typer
 
 from nextmv.cli.configuration.config import build_cloud_app
 from nextmv.cli.message import in_progress, print_json, success
-from nextmv.cli.options import AppIDOption, DebugOption, ProfileOption
+from nextmv.cli.options import AppIDOption, DebugOption, NoPaginationOption, ProfileOption
 
 # Set up subcommand application.
 app = typer.Typer()
@@ -18,6 +18,7 @@ app = typer.Typer()
 @app.command()
 def list(
     app_id: AppIDOption,
+    no_pagination: NoPaginationOption = False,
     output: Annotated[
         str | None,
         typer.Option(
@@ -34,7 +35,9 @@ def list(
     List all Nextmv Cloud batch experiments for an application.
 
     This command retrieves all batch experiments associated with the specified
-    application.
+    application. By default this command paginates the list of experiments,
+    which means multiple API calls may be made to retrieve all experiments. You
+    may use the --no-pagination option to disable pagination.
 
     [bold][underline]Examples[/underline][/bold]
 
@@ -46,11 +49,14 @@ def list(
 
     - List all batch experiments using a specific profile.
         $ [dim]nextmv cloud batch list --app-id hare-app --profile prod[/dim]
+
+    - List all experiments without pagination.
+        $ [dim]nextmv cloud batch list --app-id hare-app --no-pagination[/dim]
     """
 
     cloud_app, _ = build_cloud_app(app_id=app_id, profile=profile)
     in_progress(msg="Listing batch experiments...")
-    batch_experiments = cloud_app.list_batch_experiments()
+    batch_experiments = cloud_app.list_batch_experiments(no_pagination)
     batch_experiments_dict = [exp.to_dict() for exp in batch_experiments]
 
     if output is not None and output != "":
