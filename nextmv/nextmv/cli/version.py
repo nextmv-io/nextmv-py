fa54b76a04e947f485d56d2725d264053c40cc1e
Sebastian Quintero <sebastianquinterorojas@gmail.com>
2026-06-12 10:24:31 -0500

    Introduces function to check for the latest version of `nextmv` in PyPI

--- nextmv/nextmv/cli/version.py
+++ nextmv/nextmv/cli/version.py
@@ -2,9 +2,11 @@
 This module defines the version command for the Nextmv CLI.
 """
 
+import requests
 import typer
 
 from nextmv.__about__ import __version__
+from nextmv.cli.message import success, warning
 from nextmv.cli.options import DebugOption
 
 # Set up subcommand application.
@@ -34,6 +36,51 @@ def version_callback(value: bool):
     value : bool
         If True, print the version and exit.
     """
+
     if value:
+        latest, pypi_ver = latest_version()
+        if latest:
+            success("You are on the [magenta]latest[/magenta] version of the Nextmv CLI!")
+        else:
+            warning(
+                f"Your Nextmv CLI is outdated, the latest version is [magenta]{pypi_ver}[/magenta]. Consider updating!"
+            )
+
         print(__version__)
         raise typer.Exit()
+
+
+def latest_version() -> tuple[bool | None, str | None]:
+    """
+    This function checks if the current version of the Nextmv CLI is the latest
+    version available on PyPI.
+
+    Returns
+    -------
+    tuple[bool | None, str | None]
+        A tuple containing a boolean indicating if the current version is the
+        latest and a string with the latest version number. If there was an
+        error during the check, both values will be None.
+    """
+
+    try:
+        response = requests.get("https://pypi.org/pypi/nextmv/json")
+    except requests.RequestException:
+        return None, None
+
+    try:
+        response.raise_for_status()
+    except requests.HTTPError:
+        return None, None
+
+    if response.status_code != 200:
+        return None, None
+
+    pypi_ver = response.json().get("info", {}).get("version")
+    if pypi_ver is None:
+        return None, None
+
+    pypi_ver = f"v{pypi_ver}" if not pypi_ver.startswith("v") else pypi_ver
+
+    latest = __version__ == pypi_ver
+    return latest, pypi_ver
