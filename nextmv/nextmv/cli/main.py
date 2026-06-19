2910807653f7a62ea26377da556d496707a85f48
Marius Merschformann <marius.merschformann@gmail.com>
2026-06-19 17:58:05 +0200

    Adding note about command order

--- nextmv/nextmv/cli/main.py
+++ nextmv/nextmv/cli/main.py
@@ -57,6 +57,7 @@ app = typer.Typer(
 
 # Register subcommands. The `name` parameter is required when the subcommand
 # module has a callback function defined.
+# We want to keep the subcommands in alphabetical order.
 app.add_typer(auth_app, name="auth")
 app.add_typer(cache_app, name="cache")
 app.add_typer(cloud_app, name="cloud")
