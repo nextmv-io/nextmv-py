873b9fd041b6c4a2536dc60359eaea3a3c6d8f82
Sebastian Quintero <sebastianquinterorojas@gmail.com>
2026-07-22 17:54:35 -0500

    Fixes cache deletion logic

--- nextmv/nextmv/cli/cache/delete.py
+++ nextmv/nextmv/cli/cache/delete.py
@@ -4,8 +4,8 @@ This module defines the cache delete command for the Nextmv CLI.
 
 import typer
 
-from nextmv.cache import clear_cache, format_bytes
-from nextmv.cli.message import confirmation, info, success
+from nextmv.cache import clear_cache, format_bytes, get_cache
+from nextmv.cli.message import confirmation, in_progress, info, success
 from nextmv.cli.options import DebugOption, YesOption
 
 # Set up subcommand application.
@@ -31,11 +31,13 @@ def delete(yes: YesOption = False, _: DebugOption = False) -> None:
         $ [dim]nextmv cache delete --yes[/dim]
     """
 
-    deps, bytes_deleted = clear_cache()
-    if deps == 0 and bytes_deleted == 0:
+    cache_info = get_cache()
+    deps, total_size = cache_info["num_dependencies"], cache_info["total_size"]
+    if deps == 0 and total_size == "0 B":
         info("Nothing to delete: cache is already empty.")
         return
 
+    in_progress(f"Deleting [magenta]{deps}[/magenta] dependencies ([magenta]{total_size}[/magenta]) from the cache...")
     if not yes:
         confirm = confirmation(
             "Are you sure you want to delete the Nextmv cache? This action cannot be undone.",
@@ -45,6 +47,7 @@ def delete(yes: YesOption = False, _: DebugOption = False) -> None:
             info("The Nextmv cache will not be deleted.")
             return
 
+    deps, bytes_deleted = clear_cache()
     size_str = format_bytes(bytes_deleted)
     dep_label = "dependency" if deps == 1 else "dependencies"
     success(f"Deleted [magenta]{deps}[/magenta] {dep_label} ([magenta]{size_str}[/magenta]) from the cache.")
