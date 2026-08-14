2c24a9313b636715f44f57e927bb106a7c7feb21
Sebastian Quintero <sebastianquinterorojas@gmail.com>
2026-08-14 15:09:43 -0500

    Use explicit option names when using `typer.Option`

--- nextmv/nextmv/cli/cloud/instance/create.py
+++ nextmv/nextmv/cli/cloud/instance/create.py
@@ -83,6 +83,7 @@ def create(
     integration_id: Annotated[
         str | None,
         typer.Option(
+            "--integration-id",
             help="The integration ID to use for the runs of the instance, if applicable.",
             metavar="INTEGRATION_ID",
             rich_help_panel="Instance configuration",
@@ -111,6 +112,7 @@ def create(
     priority: Annotated[
         int,
         typer.Option(
+            "--priority",
             help="The priority of the runs in the instance. "
             "Priority is between 1 and 9, with 1 being the highest priority.",
             metavar="PRIORITY",
--- nextmv/nextmv/cli/cloud/run/logs.py
+++ nextmv/nextmv/cli/cloud/run/logs.py
@@ -44,6 +44,7 @@ def logs(
     timeout: Annotated[
         int,
         typer.Option(
+            "--timeout",
             help="The maximum time in seconds to wait for results when polling. Poll indefinitely if not set.",
             metavar="TIMEOUT_SECONDS",
         ),
